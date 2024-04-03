ddd87ce3f767519bca9e0c6f42064dda1e1a29f8
rk8287 <rounak8287@gmail.com>
2024-04-03 11:52:28 +0530

    nineth commit

---
+++ src/Pages/SearchedFlightPage/SearchedFligh.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "./SearchedFligh.css";
+import MainForm from "../../Components/MainFormSection/MainForm";
+// import mainImage from "../../Assets/Images/deeplink-bg.jpg";
+import Header from "../../Components/Header/Header";
+import RecentDeals from "../../Components/RecentCheapDeals/RecentDeals";
+import StepSection from "../../Components/StepsSection/StepSection";
+import StarSection from "../../Components/StarsSection/StarSection";
+import ReviewSection from "../../Components/ReviewsSection/ReviewSection";
+import Popup from "../../Components/Popup/Popup";
+
+const SearchedFligh = () => {
+  return (
+    <div className="searchedFlight">
+      <Popup />
+      <div className="searchedFlightContainer">
+        {/* <img className="mainImage" src={mainImage} alt="" /> */}
+        <Header />
+        <MainForm />
+      </div>
+      <div className="recentCheapDealsSection">
+        <RecentDeals />
+      </div>
+      <div className="stepSection">
+        <StepSection />
+      </div>
+      <div className="reviewAndStarSection">
+        <div className="bothSection">
+          {/* <StarSection /> */}
+          <ReviewSection />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SearchedFligh;
